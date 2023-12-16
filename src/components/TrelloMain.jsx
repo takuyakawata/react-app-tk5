7261985c364f498e6e7bbaec68bd966809d72c5a
takuyakawata <gavic.f522@gmail.com>
2023-12-16 15:10:21 +0900

    trello_1

--- src/components/TrelloMain.jsx
+++ src/components/TrelloMain.jsx
@@ -1,4 +1,4 @@
-//components/TrelloMain.jsx
+//src/components/TrelloMain.jsx
 import { useState } from 'react';
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { DummyData }from "../DummyData";
@@ -6,66 +6,72 @@ import { Card } from "./card/Card";
 
 export const TrelloMain = () => {
 
+  // ダミーデータの保管dataに入る
   const [data, setData] = useState(DummyData);
 
   const onDragEnd = (result) => {
     // console.log(result);
-    if (!result.destination) return;
+    // if (!result.destination) return;
     const { source, destination } = result;
 
-    //動かし始めたcolumnが違うcolumnに移動したら
-    if (source.droppableId !== destination.droppableId) {
-      //動かし始めたcolumnの配列の番号を取得()
-      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
-      console.log(sourceColIndex);
-      //動かし終わったcolumnの配列の番号を取得()
-      const destinationColIndex = data.findIndex(
-        (e) => e.id === destination.droppableId
-      );
-      console.log(destinationColIndex);
+  // //動かし始めたcolumnが違うcolumnに移動したら
+  if (source.droppableId !== destination.droppableId) {
 
-      const sourseCol = data[sourceColIndex];
-      const destinationCol = data[destinationColIndex];
+    //動かし始めたcolumnの配列の番号を取得()
+    const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+    console.log(sourceColIndex);
+    const sourceCol = data[sourceColIndex];
+    console.log(sourceCol);
 
-      //動かし始めたタスクに属していたカラムの中のタスクを全て取得
-      //後でsplice関数でその動かし始めたタスクを削除するため
-      //sourceTaskに配列をコピーしておく(破壊操作を後でするため)
-      const sourceTask = [...sourseCol.tasks];
-      console.log(sourceTask);
+    // 動かし終わったcolumnの配列の番号を取得()
+    const destinationColIndex = data.findIndex(
+      (e) => e.id === destination.droppableId
+    );
+    console.log(destinationColIndex);
+    const destinationCol = data[destinationColIndex];
+    console.log(destinationCol);
 
-      //動かし終わったタスクに属していたカラムの中のタスクを全て取得
-      //後でsplice関数でその動かし始めたタスクを追加するため
-      const destinationTask = [...destinationCol.tasks];
-      console.log(destinationTask);
+    //動かし始めたカラム タスクの削除
+    //sourceTaskに配列をコピーしておく(破壊操作を後でするため)
+    //後でsplice関数でその動かし始めたタスクを削除するため
+    const sourceTask = [...sourceCol.tasks];
+    console.log(sourceTask);
+    // タスクの削除
+    const [removed] = sourceTask.splice(source.index, 1);
+    // sourceTask.splice(destination.index,0,removed)
 
-      //前のカラムから削除
-      const [removed] = sourceTask.splice(source.index, 1);
-      //後のカラムに追加
-      destinationTask.splice(destination.index, 0, removed);
+    //移動した先にカラムを追加
+    //動かし終わったタスクに属していたカラムの中のタスクを全て取得
+    //後でsplice関数でその動かし始めたタスクを追加するため
+    const destinationTask = [...destinationCol.tasks];
+    console.log(destinationTask);
+    destinationTask.splice(destination.index, 0, removed);
 
-      data[sourceColIndex].tasks = sourceTask;
-      data[destinationColIndex].tasks = destinationTask;
 
-      setData(data);
-    } else {
-      //同じカラム内でタスクの入れ替え。
-      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
-      const sourseCol = data[sourceColIndex];
-      console.log(sourseCol);
-      const sourceTask = [...sourseCol.tasks];
-      console.log(sourceTask);
-      const [removed] = sourceTask.splice(source.index, 1);
-      sourceTask.splice(destination.index, 0, removed);
+    // dataの確定
+    data[sourceColIndex].tasks = sourceTask;
+    data[destinationColIndex].tasks = destinationTask;
 
-      data[sourceColIndex].tasks = sourceTask;
+    setData(data);
+      
+  } else {
+    //同じカラム内でタスクの入れ替え
+    const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+    const sourceCol = data[sourceColIndex];
+    console.log(sourceCol);
+    const sourceTask = [...sourceCol.tasks];
+    console.log(sourceTask);
+    const [removed] = sourceTask.splice(source.index, 1);
+    sourceTask.splice(destination.index, 0, removed);
 
-      setData(data);
-    }
+    data[sourceColIndex].tasks = sourceTask;
+
+    setData(data);
+  }
   };
 
-
     return (
-    <>
+      <>
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="trello">
         {data.map((section) => (
@@ -79,7 +85,7 @@ export const TrelloMain = () => {
                 <div className="trello-section-title">{section.title}</div>
                 <div className="trello-section-content">
                   {section.tasks.map((task, index) => (
-                    // <Card key={task.id}>{task.title}</Card>
+
                     <Draggable
                       key={task.id}
                       draggableId={task.id}
--- src/pages/Trello.jsx
+++ src/pages/Trello.jsx
@@ -6,7 +6,7 @@ export const Trello = () => {
     return (
       <>
         <div style={{ padding: "50px" }}>
-          <h1 style={{ marginBottom: "20px" }}>Trello風アプリ</h1>
+          <h1 style={{ marginBottom: "20px" }}>Trelloメモアプリ</h1>
           <TrelloMain />
         </div>
     </>
